fc89111b568c865f45a68668e67fd7d03118ca9d
55378 <55378@0338S-S-R0B6ZH0.sdcs.local>
2018-12-04 14:25:01 -0800

    woo

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,23 +4,221 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot yeet = new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("wut color my dood?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if(color.equals("white")) {
+			
+		}
 
         //6. If the user doesn’t enter anything, choose a random color
-
+		else {
+			boolean colour = color.equals("yellow");
+		}
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+		for (int i = 0; i < 6; i++ );
 		//4. Set the pen width to 10
-		
+		yeet.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
+yeet.penDown();
+yeet.setSpeed(50000);
+
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
+yeet.move(10);
+yeet.turn(10);
+yeet.move(10);
 
 
 	}
--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,11 +10,19 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String user = JOptionPane.showInputDialog("You like the bananananana?");
 		//2. if they say no, 
+		if(user.equalsIgnoreCase("no")) {
 			//tell them they are crazy 
+		JOptionPane.showMessageDialog(null, "Rude.");
+		}
 			//and end quiz
 		//3. if they say yes
+		if(user.equalsIgnoreCase("yes")) {
 		//	ask them what is their favorite hobby
+		  JOptionPane.showInputDialog("what is your favorite hobby?");
+		}
+	
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
 		//4. OPTIONAL: if they say something other than â€œyesâ€? or â€œnoâ€?
